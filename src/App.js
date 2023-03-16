6cc57dd393e5fef8e02e5f776ecf801063d92bcf
bachimnx <bachishermazanishvili2002@gmail.com>
2023-03-17 00:15:00 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Cart from './Cart';
+import Navbar from './Navbar';
+import Searchpage from './Searchpage';
+import ProductList from './ProductList';
+import Details from './Details';
+import ByCategory from './ByCategory';
+import Login from './Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/details" element={<Details />} />
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/search" element={<Searchpage />} />
+          <Route path="/category" element={<ByCategory />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App;
